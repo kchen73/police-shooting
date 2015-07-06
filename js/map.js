5664a9025d2313b404f03f3ab8e8b9a6249b45dc
unknown <kchen73@uw.edu>
2015-07-05 17:49:14 -0700

    rewrite

--- js/map.js
+++ js/map.js
@@ -1,33 +1,24 @@
 // Function to draw your map
 var drawMap = function() {
-
-  var map = L.map('container')
- 
-  map.setView([39.8282,-98.5795], 4)
-  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
-
-  layer.addTo(map)
- 
-
-  // Execute your function to get data
- 
+	var map = L.map('container')
+	map.setView([39.8282, -98.5795], 4)
+	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
+	layer.addTo(map)
 }
 
-
+  // Execute your function to get data
 
 var data;
 $.ajax({
-     url:'data/response.json',
-     type: "get",
-     success: function(dat) {
-       data = dat
-       data.map(function(d) {
-         var circle = new L.circle([d.lat, d.lng], 
-         20000, {color:'red', opacity:.5}).addTo(map)
-       })
-     }, 
-     dataType:"json"
-}) 
+	url: 'data/response.json', 
+	type: 'get', 
+	success: function(dat) {
+		data = datdata.map(function(d) {
+			var circle = new L.circle([d.lat, d.lng], 20000, {color: 'red', opacity: .5}).addTo(map)
+		})
+	}, 
+	dataType: 'json'
+})
 
 /*
 
