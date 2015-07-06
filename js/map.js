20d5d4dc486f290e4846d92b8ff3a3695e75bd49
unknown <kchen73@uw.edu>
2015-07-05 17:23:05 -0700

    map fix?

--- js/map.js
+++ js/map.js
@@ -3,15 +3,15 @@ var drawMap = function() {
 
   var map = L.map('container');
  
-  map.setView([39.8282,-98.5795],4);
-  layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+  map.setView([39.8282,-98.5795], 4);
+  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 
   layer.addTo(map);
  
 
   // Execute your function to get data
  
-};
+}
 
 
 
@@ -20,14 +20,14 @@ $.ajax({
      url:'data/response.json',
      type: "get",
      success: function(dat) {
-       data = dat;
+       data = dat
        data.map(function(d) {
-         var circle = new L.circleMarker([d.lat, d.lng], 
-         200000, {color:'red', opacity:.5}).addTo(map)
-       });
+         var circle = new L.circle([d.lat, d.lng], 
+         20000, {color:'red', opacity:.5}).addTo(map)
+       })
      }, 
      dataType:"json"
-}); 
+}) 
 
 /*
 
