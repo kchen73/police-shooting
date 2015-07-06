69dad2dab085f195c7c33a0f5a63ac9c4fe0f4db
unknown <kchen73@uw.edu>
2015-07-05 18:47:18 -0700

    popup test

--- js/map.js
+++ js/map.js
@@ -1,35 +1,32 @@
-// Function to draw your map
 var drawMap = function() {
 	var map = L.map('container')
 	map.setView([39.8282, -98.5795], 4)
 	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
 	layer.addTo(map)
+
+    // Execute your function to get data
+
+    var data;
+    $.ajax({
+	    url: 'data/response.json', 
+	    type: 'get', 
+	    success: function(dat) {
+		    data = dat
+		    data.map(function(d) {
+			    var circle = new L.circle([d.lat, d.lng], 2345, {color: 'green', opacity: .5}).addTo(map)
+                var state = d.state
+			    circle.bindPopup(state)
+		    })
+	    }, 
+	    dataType: 'json'
+    })
 }
 
-  // Execute your function to get data
-
-var data;
-$.ajax({
-	url: 'data/response.json', 
-	type: 'get', 
-	success: function(dat) {
-		data = dat
-		var map
-		data.map(function(d) {
-			var circle = new L.circle([d.lat, d.lng], 20000, {color: 'red', opacity: .5}).addTo(map)
-		})
-	}, 
-	dataType: 'json'
-})
-
 /*
-
-// Do something creative with the data here!  
 var customBuild = function() {
 
   
 }
-
 */
 
 
