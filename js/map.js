e404c4b3af26db1e3a0da75425bc601669ee5301
unknown <kchen73@uw.edu>
2015-07-06 04:18:28 -0700

    basic

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 var drawMap = function() {
-	var map = L.map('container')
-	map.setView([39.8282, -98.5795], 4)
-	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
-	layer.addTo(map)
+	var map = L.map('container');
+	map.setView([39.8282, -98.5795], 4);
+	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+	layer.addTo(map);
 
     // Execute your function to get data
 
@@ -11,24 +11,18 @@ var drawMap = function() {
 	    url: 'data/response.json', 
 	    type: 'get', 
 	    success: function(dat) {
-		    data = dat
+		    data = dat;
 		    data.map(function(d) {
-			    var circle = new L.circle([d.lat, d.lng], 2345, {color: 'green', opacity: .5}).addTo(map)
-                var state = d.state
-			    circle.bindPopup(state)
-		    })
+			    var circle = new L.circleMarker([d.lat, d.lng], {radius: 3, color: 'green', opacity: 0.5}).addTo(map);
+                var race = d.Race;
+                var weapon = d.Weapon;
+                circle.bindPopup('<b>Race:</b> ' + race + ' <br><b>Weapon:</b> ' + weapon);
+
+		    });
 	    }, 
 	    dataType: 'json'
-    })
-}
-
-/*
-var customBuild = function() {
-
-  
-}
-*/
-
+    });
+};
 
 
 
