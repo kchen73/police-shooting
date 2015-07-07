6d47d846f850c223486af0df056bf447ee619269
unknown <kchen73@uw.edu>
2015-07-06 21:57:45 -0700

    final changes

--- js/map.js
+++ js/map.js
@@ -3,6 +3,13 @@ var drawMap = function() {
 	map.setView([39.8282, -98.5795], 4);
 	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 	layer.addTo(map);
+<<<<<<< HEAD
+	getData(map)
+}
+
+var getData = function(map) {
+=======
+>>>>>>> 29cb4828f4fe2d69148319a284d3f3594a398aa6
     var data;
     getData(map);
 }
@@ -14,41 +21,44 @@ var getData = function(map) {
 	    success: function(dat) {
 		    data = dat;
 		    data.map(function(d) {
-			    var race = d.Race;
-                var weapon = d.Weapon;
                 var circle = new L.circleMarker([d.lat, d.lng], {radius: 1, color: 'black', opacity: 0.1}).addTo(map);
-
-                if (d.lng < -116) {
-                	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'yellow', opacity: 0.7}).addTo(map);
-                }
-                else if (d.lng < -103) {
-                	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'purple', opacity: 0.7}).addTo(map);
-                }
-                else if (d.lng < -93) {
-                	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'orange', opacity: 0.7}).addTo(map);
-                }
-                else if (d.lat < 37) {
-                	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'red', opacity: 0.7}).addTo(map);
-                }
-                else if (d.lng < -80) {
-                	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'blue', opacity: 0.7}).addTo(map);
-                }
-                else {
- 					var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'green', opacity: 0.7}).addTo(map);               	
-                }
-
-                if (race === 'Unknown') {
-                	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'white', opacity: 0.1}).addTo(map);
-                }
-                else if (race === 'White') {
-                    var circle = new L.circleMarker([d.lat, d.lng], {radius: 4, color: 'black', opacity: 0.6}).addTo(map);
-                }
-                else {
-                	var circle = new L.circleMarker([d.lat, d.lng], {radius: 4, color: 'black', opacity: 1}).addTo(map);
-                }
-                circle.bindPopup('<b>Race:</b> ' + race + ' <br><b>Weapon:</b> ' + weapon);
+                customBuild(d, map);          
 		    });
 	    }, 
 	    dataType: 'json'
     });
 };
+
+var customBuild = function(d, map) {
+	var race = d.Race;
+    var weapon = d.Weapon;
+	if (d.lng < -116) {
+    	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'yellow', opacity: 0.7}).addTo(map);
+    }
+    else if (d.lng < -103) {
+    	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'purple', opacity: 0.7}).addTo(map);
+    }
+    else if (d.lng < -93) {
+    	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'orange', opacity: 0.7}).addTo(map);
+    }
+    else if (d.lat < 37) {
+    	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'red', opacity: 0.7}).addTo(map);
+    }
+    else if (d.lng < -80) {
+    	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'blue', opacity: 0.7}).addTo(map);
+    }
+    else {
+		var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'green', opacity: 0.7}).addTo(map);               	
+    }
+
+    if (race === 'Unknown') {
+    	var circle = new L.circleMarker([d.lat, d.lng], {radius: 2, color: 'white', opacity: 0.1}).addTo(map);
+    }
+    else if (race === 'White') {
+        var circle = new L.circleMarker([d.lat, d.lng], {radius: 4, color: 'black', opacity: 0.4}).addTo(map);
+    }
+    else {
+    	var circle = new L.circleMarker([d.lat, d.lng], {radius: 4, color: 'black', opacity: 1}).addTo(map);
+    }
+    circle.bindPopup('<b>Race:</b> ' + race + ' <br><b>Weapon:</b> ' + weapon);
+}
